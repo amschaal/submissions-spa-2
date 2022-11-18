1f1451e95473236796d919170eec0cfa83318ed6
Adam Schaal <amschaal@gmail.com>
2022-11-18 12:04:12 -0800

    Added simple test plugin

---
+++ src/assets/plugins/test/Test.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    Config: {{config}}
+    Submission: {{submission.id}}
+  </div>
+</template>
+
+<script>
+export default {
+  props: ['submission', 'config'],
+  data () {
+    return {
+    }
+  },
+  methods: {
+  },
+  mounted () {
+  },
+  computed: {
+  }
+}
+</script>
