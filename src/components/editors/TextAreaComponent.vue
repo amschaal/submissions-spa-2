d030e2c865f03624150942d34f14e18402de7710
Adam Schaal <amschaal@gmail.com>
2023-09-29 11:16:06 -0700

    change form field spacing, add textarea widget #24

---
+++ src/components/editors/TextAreaComponent.vue
@@ -0,0 +1,30 @@
+<template>
+    <q-input
+      v-model="text"
+      filled
+      type="textarea"
+      style="width:100%"
+      @input="onInput"
+    />
+</template>
+<script>
+import Vue from 'vue'
+
+export default Vue.extend({
+  props: ['value'],
+  data () {
+    return {
+      text: this.value
+    }
+  },
+  methods: {
+    onInput () {
+      this.$emit('input', this.text)
+    }
+  },
+  mounted () {
+    // Vue.nextTick(() => {
+    // })
+  }
+})
+</script>
