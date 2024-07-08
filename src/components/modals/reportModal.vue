f7b93809d34697ede8978b26c42aebcf315433c0
Adam Schaal <amschaal@gmail.com>
2024-07-08 08:11:14 -0700

    Make list of reports sortable and searchable

--- src/components/modals/reportModal.vue
+++ src/components/modals/reportModal.vue
@@ -8,26 +8,37 @@
           <div class="text-h6">Reports</div>
         </q-card-section>
 
-        <q-card-section class="q-pt-none" v-on:selected="updateSelected">
+        <q-card-section class="q-pt-none">
           <!-- qs: {{ qs }}
           {{ reports }} -->
-          <table v-if="reports">
-            <thead>
-              <tr><th>Name</th><th>Description</th></tr>
-            </thead>
-            <tbody>
-              <tr v-for="r in reports" :key="r.id">
-                <td>{{ r.name }}</td>
-                <td>{{ r.description }}</td>
-              </tr>
-            </tbody>
-          </table>
+          <q-table
+            :data="reports"
+            :columns="columns"
+            :filter="filter"
+            row-key="id"
+            binary-state-sort
+            :rows-per-page-options="[10,25,0]"
+            v-if="reports"
+          >
+            <template v-slot:top-right>
+              <q-input borderless dense debounce="300" v-model="filter" placeholder="Search">
+                <template v-slot:append>
+                  <q-icon name="search" />
+                </template>
+              </q-input>
+            </template>
+            <template v-slot:body-cell-action="props">
+              <q-td :props="props">
+                <q-btn flat label="Export" /> <q-btn color="primary" flat label="View" />
+              </q-td>
+            </template>
+          </q-table>
           <h3 v-else>
             Loading reports...
           </h3>
         </q-card-section>
         <q-card-actions align="right" class="bg-white text-teal">
-          <q-btn flat label="Export" @click="select" /> <q-btn flat label="Create" @click="create" />  <q-btn flat label="Cancel" @click="cancel" />
+         <q-btn flat label="Cancel" @click="cancel" />
         </q-card-actions>
       </q-card>
     </q-dialog>
@@ -41,7 +52,18 @@ export default {
     return {
       qs: '',
       opened: false,
-      reports: null
+      reports: null,
+      filter: null,
+      columns: [
+        { name: 'name', align: 'center', label: 'Name', field: 'name', sortable: true },
+        { name: 'description', align: 'center', label: 'Description', field: 'description', sortable: false },
+        { name: 'action', align: 'center', label: 'Action', field: 'name', sortable: false }
+      ],
+      pagination: {
+        sortBy: 'name',
+        descending: true,
+        rowsPerPage: 10
+      }
     }
   },
   mounted () {
