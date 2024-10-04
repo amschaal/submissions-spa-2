9a1a4e3906d1edfa9abae8883b72bb4d8cb55b8d
Adam Schaal <amschaal@gmail.com>
2024-10-04 15:18:01 -0700

    creating a component for visualizing JSON schema changes #35

---
+++ src/components/jsonDiff.vue
@@ -0,0 +1,56 @@
+<template>
+  <div>
+    <q-checkbox v-model="showUnchanged" label="Show Unchanged"/>
+    <div v-html="renderedDiff"  :class="showUnchanged ? 'show-unchanged' : 'hide-unchanged'"></div>
+  </div>
+
+</template>
+
+<script>
+import * as jsondiffpatch from 'jsondiffpatch'
+import * as htmlFormatter from 'jsondiffpatch/lib/formatters/html'
+const JD = jsondiffpatch.create({})
+export default {
+  props: ['left', 'right', 'diff'],
+  data () {
+    return {
+      renderedDiff: '',
+      showUnchanged: false
+    }
+  },
+  methods: {
+    updateDiff (left, right) {
+      const diff = JD.diff(left || this.left, right || this.right)
+      this.renderedDiff = htmlFormatter.format(diff, left || this.left)
+    }
+  },
+  mounted () {
+    const diff = this.diff || JD.diff(this.left, this.right)
+    this.renderedDiff = htmlFormatter.format(diff, this.left)
+  },
+  watch: { // This could be inefficient if many diffs, or diffs of large objects are active.  Perhaps it would be better to require the parent to explicitly call the updateDiff method.
+    left: {
+      handler () {
+        this.updateDiff()
+      },
+      deep: true
+    },
+    right: {
+      handler () {
+        this.updateDiff()
+      },
+      deep: true
+    }
+  }
+}
+</script>
+
+<style src="jsondiffpatch/lib/formatters/styles/html.css"></style>
+<style>
+.jsondiffpatch-unchanged {
+  display: none;
+}
+.show-unchanged .jsondiffpatch-unchanged {
+  display: block;
+}
+</style>
