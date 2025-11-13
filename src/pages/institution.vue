439e86b1aab9dca9c741a892388d6934fe3e58a9
Adam Schaal <amschaal@gmail.com>
2025-11-12 16:49:21 -0800

    Add institution groups and submissions

--- src/pages/institution.vue
+++ src/pages/institution.vue
@@ -8,7 +8,30 @@
 
       <q-separator />
       <q-card-section>
-       <group-table :query-params="`institution__id=${this.id}`"></group-table>
+        <q-tabs
+          v-model="tab"
+          dense
+          class="bg-primary text-grey shadow-2"
+          active-color="white"
+          align="justify"
+          narrow-indicator
+        >
+          <q-tab name="groups"  default>Groups</q-tab>
+          <q-tab name="submissions" label="Submissions"/>
+        </q-tabs>
+        <q-tab-panels v-model="tab" animated :keep-alive="true">
+          <q-tab-panel name="groups">
+            <q-card-section>
+            <group-table :query-params="`institution__id=${this.id}`"></group-table>
+            </q-card-section>
+          </q-tab-panel>
+          <q-tab-panel name="submissions">
+            <q-card-section>
+            <submissions :query-params="`pi__institution__id=${id}`" v-if="id"></submissions>
+            </q-card-section>
+          </q-tab-panel>
+        </q-tab-panels>
+
     </q-card-section>
   </q-card>
   </q-page>
@@ -19,11 +42,13 @@
 
 <script>
 import GroupTable from '../components/tables/GroupTable.vue'
+import submissions from '../components/submissions.vue'
 export default {
   props: ['id'],
   data () {
     return {
-      'institution': null
+      'institution': null,
+      'tab': 'groups'
     }
   },
   mounted () {
@@ -34,7 +59,8 @@ export default {
   },
   name: 'Institution',
   components: {
-    GroupTable
+    GroupTable,
+    submissions
   }
 }
 </script>
